0f1164b98dc178e8692f8060f28a30d4922af920
skylee99 <juliuslee.sky99@gmail.com>
2021-03-25 02:23:33 +0600

    test file upload image_picker

--- lib/views/store_selection_page/view.dart
+++ lib/views/store_selection_page/view.dart
@@ -1,4 +1,5 @@
 import 'package:com.floridainc.dosparkles/utils/general.dart';
+import 'package:com.floridainc.dosparkles/widgets/test_image_picker.dart';
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -26,7 +27,9 @@ Widget buildView(
       ],
     ),
     appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(60), child: _AppBar()),
+      preferredSize: const Size.fromHeight(60),
+      child: _AppBar(),
+    ),
     drawer: SparklesDrawer(),
   );
 }
